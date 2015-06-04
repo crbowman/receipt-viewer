1885ee6f5e35ee480b73934b6d0464ac9b330093
narwally <c.bowman1711@gmail.com>
2015-06-04 13:13:56 -0400

    add new route

--- viewer.py
+++ viewer.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+
+@app.route('/annotate')
+def annotate():
     return render_template('product.html')
+
+@app.route('/'):
+	return render_template('home.html', my_string='home template', my_list['receipt 1', 'receipt 2', 'receipt 3'])
