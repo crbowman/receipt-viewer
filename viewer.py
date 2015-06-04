2cfad7cdefec21bf79e4b2b5ccdce54477885244
narwally <c.bowman1711@gmail.com>
2015-06-04 13:18:40 -0400

    fix syntax error in viewer.py

--- viewer.py
+++ viewer.py
@@ -9,4 +9,5 @@ def annotate():
     return render_template('product.html')
 
 @app.route('/'):
+def home():
 	return render_template('home.html', my_string='home template', my_list['receipt 1', 'receipt 2', 'receipt 3'])
